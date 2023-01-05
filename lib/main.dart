d7e095ae750e58cd19f7eb07e8e20138bf306e9f
Daniil Frank <orgfrank@icloud.com>
2023-01-05 14:15:03 +0600

    Setting Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layaout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCYbirTvNb0sNLnvNibqCAslYH5Gb3dz1s",
+            authDomain: "instagramflutter-ede55.firebaseapp.com",
+            projectId: "instagramflutter-ede55",
+            storageBucket: "instagramflutter-ede55.appspot.com",
+            messagingSenderId: "138023735994",
+            appId: "1:138023735994:web:3e18cabe3f1406764da883"));
+  } else {
+    await Firebase.initializeApp();
+  }
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCYbirTvNb0sNLnvNibqCAslYH5Gb3dz1s",
+          authDomain: "instagramflutter-ede55.firebaseapp.com",
+          projectId: "instagramflutter-ede55",
+          storageBucket: "instagramflutter-ede55.appspot.com",
+          messagingSenderId: "138023735994",
+          appId: "1:138023735994:web:3e18cabe3f1406764da883"));
   runApp(const MyApp());
 }
 
