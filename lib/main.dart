1f60e570ca9bb34856815d641318f966b449fe3d
Daniil Frank <orgfrank@icloud.com>
2023-01-07 15:46:50 +0600

    Create sign up screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layaout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
+import 'package:instagram_flutter/screens/sing_up_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 void main() async {
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
           //     webScreenLayout: WebScreenLayaout(),
           //     mobileScreenLayout: MobileScreenLayout()),
 
-          body: LoginScreen(),
+          body: SingUpScreen(),
         ));
   }
 }
--- lib/screens/sing_up_screen.dart
+++ lib/screens/sing_up_screen.dart
@@ -3,22 +3,26 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SingUpScreen extends StatefulWidget {
+  const SingUpScreen({Key? key}) : super(key: key);
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _SingUpScreenState createState() => _SingUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SingUpScreenState extends State<SingUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -39,8 +43,36 @@ class _LoginScreenState extends State<LoginScreen> {
             color: primaryColor,
             height: 64,
           ),
+          SizedBox(
+            height: 64,
+          ),
+          //circular widget to accept and show our selected file
+          Stack(
+            children: [
+              const CircleAvatar(
+                radius: 64,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1672934033380-cb7b08eb46b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=796&q=80'),
+              ),
+              Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo),
+                  ))
+            ],
+          ),
+          const SizedBox(height: 24),
+          //text field input for username
+          TextFieldInput(
+              textEditingController: _usernameController,
+              hintText: 'Enter your username',
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
           //text field input for email
-          const SizedBox(height: 64),
           TextFieldInput(
               textEditingController: _emailController,
               hintText: 'Enter your email',
@@ -55,13 +87,21 @@ class _LoginScreenState extends State<LoginScreen> {
             textInputType: TextInputType.text,
             isPass: true,
           ),
+          //text field input for bio
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _bioController,
+              hintText: 'Enter your bio',
+              textInputType: TextInputType.text),
           //button for login
           const SizedBox(
             height: 24,
           ),
           InkWell(
             child: Container(
-              child: const Text('Log in'),
+              child: const Text('Sing up'),
               width: double.infinity,
               alignment: Alignment.center,
               padding: const EdgeInsets.symmetric(vertical: 12),
