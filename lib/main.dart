f3a7c1549a29f79c5c30ea476fa78bc2c46f5cef
Daniil Frank <orgfrank@icloud.com>
2023-01-16 00:05:07 +0600

    Modelling user data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -37,11 +38,32 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark()
             .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
         home: Scaffold(
-          // body: const ResponsiveLayout(
-          //     webScreenLayout: WebScreenLayaout(),
-          //     mobileScreenLayout: MobileScreenLayout()),
+          body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  return const ResponsiveLayout(
+                      webScreenLayout: WebScreenLayaout(),
+                      mobileScreenLayout: MobileScreenLayout());
+                } else if (snapshot.hasError) {
+                  return Scaffold(
+                    body: Center(child: Text('${snapshot.error}')),
+                  );
+                }
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Scaffold(
+                  body: const Center(
+                      child: CircularProgressIndicator(
+                    color: primaryColor,
+                  )),
+                );
+              }
 
-          body: LoginScreen(),
+              return LoginScreen();
+            },
+          ),
         ));
   }
 }
